7cad4a9c58cdb127897308ce26ad23a9d40729e2
jeff <jeffhogg86@gmail.com>
2021-03-17 18:28:50 -0700

    remove pdf viewer

--- components/Resume.tsx
+++ components/Resume.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image';
 import { Container } from '@material-ui/core';
-import '@react-pdf-viewer/core/lib/styles/index.css';
 import styles from '../styles/Resume.module.css';
 
 const Resume = () => {
