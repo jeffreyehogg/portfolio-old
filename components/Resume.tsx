1e2464a07efa2c8cf6cd7a17a49af9671b7c7eba
jeff <jeffhogg86@gmail.com>
2021-03-17 18:24:25 -0700

    add canva resume, about paragaraph

--- components/Resume.tsx
+++ components/Resume.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { Container } from '@material-ui/core';
 import { Viewer, Worker } from '@react-pdf-viewer/core';
 import '@react-pdf-viewer/core/lib/styles/index.css';
@@ -6,9 +7,12 @@ import styles from '../styles/Resume.module.css';
 const Resume = () => {
   return (
     <Container className={styles.container}>
-      <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js">
-        <Viewer fileUrl="/HoggResume.pdf" />
-      </Worker>
+      <Image
+        src="/jeffhoggresume.png"
+        alt="Resume Picture"
+        width={1545}
+        height={2000}
+      />
     </Container>
   );
 };
