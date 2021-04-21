842a69907a3c656c67e0061d7257d04c12f18255
jeff <jeffhogg86@gmail.com>
2021-04-21 15:19:58 -0700

    adds resume

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,5 +10,8 @@ module.exports = {
     },
   },
   variants: {},
-  plugins: [require('@tailwindcss/aspect-ratio')],
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+  ],
 };
