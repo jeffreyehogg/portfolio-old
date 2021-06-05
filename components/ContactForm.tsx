4b4f9479b0ea9af48f10f1d58a4c53c3f21e5712
jeff <jeffhogg86@gmail.com>
2021-06-04 21:32:18 -0700

    remove email

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -6,40 +6,8 @@ export default function Example() {
       <div className="absolute inset-0">
         <div className="absolute inset-y-0 left-0 w-1/2 bg-gray-50" />
       </div>
-      <div className="relative max-w-7xl mx-auto lg:grid lg:grid-cols-5">
-        <div className="bg-gray-50 py-16 px-4 sm:px-6 lg:col-span-2 lg:px-8 lg:py-24 xl:pr-12">
-          <div className="max-w-lg mx-auto">
-            <h2 className="text-2xl font-extrabold tracking-tight text-gray-900 sm:text-3xl">
-              Contact me
-            </h2>
-            <p className="mt-3 text-lg leading-6 text-gray-500">
-              Feel free to send me a message or reach out directly by phone or
-              email.
-            </p>
-            <dl className="mt-8 text-base text-gray-500">
-              <div className="mt-3">
-                <dt className="sr-only">Email</dt>
-                <dd className="flex">
-                  <MailIcon
-                    className="flex-shrink-0 h-6 w-6 text-gray-400"
-                    aria-hidden="true"
-                  />
-                  <span className="ml-3">
-                    {' '}
-                    <a
-                      href="mailto:jeffhogg86@gmail.com"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      jeffhogg86@gmail.com
-                    </a>
-                  </span>
-                </dd>
-              </div>
-            </dl>
-          </div>
-        </div>
-        <div className="bg-white py-16 px-4 sm:px-6 lg:col-span-3 lg:py-24 lg:px-8 xl:pl-12">
+      <div className="relative max-w-xl mx-auto">
+        <div className="bg-white pt-14 lg:col-span-3">
           <div className="max-w-lg mx-auto lg:max-w-none">
             <form action="#" method="POST" className="grid grid-cols-1 gap-y-6">
               <div>
