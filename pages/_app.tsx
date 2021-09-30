7eb02b7953bade1f026c928283fcc0e7d19999f1
jeffhogg86 <jeffhogg86@gmail.com>
2021-09-30 16:32:47 -0700

    remove tag manager module

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,7 @@
-import { useEffect } from 'react'
 import TagManager from 'react-gtm-module'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: any) {
-  useEffect(() => {
-    TagManager.initialize({ gtmId: process.env.NEXT_PUBLIC_GA_ID.toString() })
-  }, [])
   return <Component {...pageProps} />
 }
 
