ecf21efa5cb1f86139e4a2117528b935c1d8d596
jeffhogg86 <jeffhogg86@gmail.com>
2021-09-30 16:28:40 -0700

    adds google tag manager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
+import { useEffect } from 'react'
+import TagManager from 'react-gtm-module'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: any) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: process.env.NEXT_PUBLIC_GA_ID.toString() })
+  }, [])
   return <Component {...pageProps} />
 }
 
