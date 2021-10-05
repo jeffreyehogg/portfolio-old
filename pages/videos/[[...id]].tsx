e60fe8beb2924b592000cde2f664ede2e9a06a5d
jeffhogg86 <jeffhogg86@gmail.com>
2021-10-05 10:24:50 -0700

    edit path

--- pages/videos/[[...id]].tsx
+++ pages/videos/[[...id]].tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Layout from '../../components/Layout'
 import VideoList from '../../components/VideoList'
 import VideoPlayer from '../../components/VideoPlayer'
-import { YOUTUBE_ALL_VIDEOS_API } from '../../lib/videoConstants'
+import { YOUTUBE_ALL_VIDEOS_API } from '../../lib/constants'
 
 export default function Videos({ data }: any) {
   return (
