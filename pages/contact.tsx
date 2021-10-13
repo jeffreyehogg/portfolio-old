303259b7c4a0344c4d38eb4aca8a5afa0aba4e6a
jeffhogg86 <jeffhogg86@gmail.com>
2021-10-13 08:47:26 -0700

    contact form sends email

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,10 +1,18 @@
 import ContactForm from '../components/ContactForm'
+import ContactInfo from '../components/ContactInfo'
 import Layout from '../components/Layout'
 
 function contact() {
   return (
     <Layout title='Contact'>
-      <ContactForm />
+      <div className='bg-gray-100 max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8'>
+        <div className='relative bg-white rounded-md shadow-xl'>
+          <div className='grid grid-cols-1 lg:grid-cols-3'>
+            <ContactInfo />
+            <ContactForm />
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
