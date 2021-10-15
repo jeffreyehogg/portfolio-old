759ee81278141d74c0eda221e5cd2ca67b8eedda
jeffhogg86 <jeffhogg86@gmail.com>
2021-10-15 12:34:35 -0700

    fix form button layout

--- client/components/ContactForm.tsx
+++ client/components/ContactForm.tsx
@@ -195,20 +195,26 @@ export default function ContactForm() {
             />
           </div>
         </div>
-        <div className='sm:col-span-2 flex justify-between'>
-          <Recaptcha
-            sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
-            render='explicit'
-            onloadCallback={recaptchaLoaded}
-            verifyCallback={verifiedRecaptcha}
-          />
-          <button
-            type='submit'
-            onClick={handleSubmit}
-            className='mt-2 w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-400 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:w-auto'
-          >
-            Submit
-          </button>
+        <div className='col-span-1 md:col-span-2 flex justify-between'>
+          <div className='flex items-center'>
+            <Recaptcha
+              sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
+              render='explicit'
+              onloadCallback={recaptchaLoaded}
+              verifyCallback={verifiedRecaptcha}
+            />
+          </div>
+        </div>
+        <div className='col-span-1 md:col-span-2'>
+          <div className='flex items-center'>
+            <button
+              type='submit'
+              onClick={handleSubmit}
+              className='mt-2 w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-400 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:w-auto'
+            >
+              Submit
+            </button>
+          </div>
         </div>
       </form>
     </div>
