d1b18ccca4fc2a0c14443db74b1c99ace45a759c
jeffhogg86 <jeffhogg86@gmail.com>
2021-10-20 15:57:45 -0700

    fix button

--- client/components/Hero.tsx
+++ client/components/Hero.tsx
@@ -33,10 +33,10 @@ export default function Hero() {
                 Portfolio
               </a>
               <a
-                href='/contact'
+                href='/about'
                 className='flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-indigo-700 bg-white hover:bg-indigo-50 sm:px-8'
               >
-                Contact
+                About
               </a>
             </div>
           </div>
