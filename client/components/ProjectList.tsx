944c76009343802de4002e7aee9fad3f2c7f1945
jeffhogg86 <jeffhogg86@gmail.com>
2021-10-20 19:24:47 -0700

    add github links and buttons

--- client/components/ProjectList.tsx
+++ client/components/ProjectList.tsx
@@ -8,14 +8,16 @@ const projects = [
       'An online marketplace for sportscards. Built with Next.js and Redux. When complete, user will be able to create an account, add items to shopping cart and checkout with PayPal.',
 
     imageUrl: '/images/cardshop.png',
+    githubUrl: 'https://github.com/jeffhogg86/cardshop',
   },
   {
     title: 'Iron Dojo',
     href: 'https://the-iron-dojo.herokuapp.com/',
     description:
-      '(Initial load is slow) A react web application built to track your strength and conditioning sessions over time. Create and log your workouts, view your progress in a chart.',
+      '(Initial load is slow) A react web application built to track your strength and conditioning over time. Create and log your workouts, view your progress in a chart.',
 
     imageUrl: '/images/ironDojo-ss.png',
+    githubUrl: 'https://github.com/jeffhogg86/Iron-Dojo',
   },
   {
     title: 'Hogg Media',
@@ -24,6 +26,7 @@ const projects = [
       'A static blog/vlog site built with Next.js. Uses Next.js, Server-Side Rendering, TailwindCSS, YouTube API, Markdown.',
 
     imageUrl: '/images/hoggmedia.png',
+    githubUrl: 'https://github.com/jeffhogg86/hoggmedia',
   },
 
   {
@@ -33,6 +36,7 @@ const projects = [
       'A Discord style chat app built with react. Demonstrates basic chat functionality, creating a channel and sending messages. Implements Google Oauth for login.',
 
     imageUrl: '/images/chatter.png',
+    githubUrl: 'https://github.com/jeffhogg86/chatter',
   },
   {
     title: 'Shoflix',
@@ -41,6 +45,7 @@ const projects = [
       "A Netflix style video previewer. Makes an API call to render movies and shows by category. Some of the movie/show's play a trailer on a click. Deployed with firebase.",
 
     imageUrl: '/images/shoflix.png',
+    githubUrl: 'https://github.com/jeffhogg86/shoflix',
   },
   {
     title: 'Hoggle Search',
@@ -49,6 +54,7 @@ const projects = [
       'A Google type search engine built with Next.js, Tailwind and the Google Search Api. Deployed with vercel.',
 
     imageUrl: '/images/hoggle.png',
+    githubUrl: 'https://github.com/jeffhogg86/hoggle-search',
   },
 ]
 
@@ -69,34 +75,40 @@ export default function ProjectList() {
           {projects.map((post) => (
             <div
               key={post.title}
-              className='flex flex-col rounded-lg shadow-lg overflow-hidden cursor-pointer'
+              className='flex flex-col rounded-lg shadow-lg overflow-hidden'
             >
               <div className='flex-shrink-0'>
-                <a href={post.href} target='_blank' rel='noreferrer'>
-                  <Image
-                    className='h-48 w-full object-cover'
-                    width={400}
-                    height={200}
-                    src={post.imageUrl}
-                    alt='Project screenshot'
-                  />
-                </a>
+                <Image
+                  className='h-48 w-full object-cover'
+                  width={400}
+                  height={200}
+                  src={post.imageUrl}
+                  alt='Project screenshot'
+                />
               </div>
               <div className='flex-1 bg-white p-6 flex flex-col justify-between'>
-                <div className='flex-1'>
-                  <p className='text-sm font-medium text-indigo-600'></p>
+                <p className='text-sm font-medium text-indigo-600'></p>
+                <p className='text-xl font-semibold text-gray-900'>
+                  {post.title}
+                </p>
+                <p className='mt-3 text-base text-gray-500'>
+                  {post.description}
+                </p>
+
+                <div className='mt-6 flex space-x-4'>
                   <a
                     href={post.href}
                     target='_blank'
-                    rel='noreferrer'
-                    className='block mt-2'
+                    className='inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md shadow-sm text-white bg-indigo-700  hover:bg-opacity-70 transition ease-in-out duration-150 ml-2'
                   >
-                    <p className='text-xl font-semibold text-gray-900'>
-                      {post.title}
-                    </p>
-                    <p className='mt-3 text-base text-gray-500'>
-                      {post.description}
-                    </p>
+                    View Live
+                  </a>
+                  <a
+                    href={post.githubUrl}
+                    target='_blank'
+                    className='inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md shadow-sm text-white bg-indigo-700  hover:bg-opacity-70 transition ease-in-out duration-150'
+                  >
+                    View on Github
                   </a>
                 </div>
               </div>
