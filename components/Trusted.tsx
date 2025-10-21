ba447e5c06f413b6b7a12552eaffd52cd28afab9
Jeff Hogg <jeffhogg86@gmail.com>
2025-10-20 20:36:25 -0500

    add motion

--- components/Trusted.tsx
+++ components/Trusted.tsx
@@ -1,8 +1,15 @@
 import Image from 'next/image'
+import { motion } from 'framer-motion'
 
 const Trusted = () => {
 	return (
-		<div className='bg-white pt-12 sm:pt-16'>
+		<motion.div
+			className='bg-white pt-12 sm:pt-16'
+			initial={{ opacity: 0, y: 50 }}
+			whileInView={{ opacity: 1, y: 0 }}
+			viewport={{ once: true, amount: 0.3 }}
+			transition={{ duration: 0.6 }}
+		>
 			<div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
 				<div className='max-w-4xl mx-auto text-center'>
 					<h2 className='text-3xl font-extrabold text-gray-900 sm:text-4xl'>
@@ -15,6 +22,7 @@ const Trusted = () => {
 			</div>
 			<div className='max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8'>
 				<div className='grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-4'>
+					{/* Logo 1: Cisco */}
 					<div className='col-span-1 flex justify-center md:col-span-2 lg:col-span-1 transition-transform duration-300 ease-in-out hover:scale-105 cursor-pointer'>
 						<Image
 							width={200}
@@ -23,6 +31,7 @@ const Trusted = () => {
 							alt='Cisco'
 						/>
 					</div>
+					{/* Logo 2: 49ers */}
 					<div className='col-span-1 flex justify-center md:col-span-2 lg:col-span-1 transition-transform duration-300 ease-in-out hover:scale-105 cursor-pointer'>
 						<Image
 							width={170}
@@ -31,6 +40,7 @@ const Trusted = () => {
 							alt='San Francisco 49ers'
 						/>
 					</div>
+					{/* Logo 3: Giants */}
 					<div className='col-span-1 flex justify-center md:col-span-2 lg:col-span-1 transition-transform duration-300 ease-in-out hover:scale-105 cursor-pointer'>
 						<Image
 							width={200}
@@ -39,6 +49,7 @@ const Trusted = () => {
 							alt='San Francisco Giants'
 						/>
 					</div>
+					{/* Logo 4: Warriors */}
 					<div className='col-span-1 flex justify-center md:col-span-3 lg:col-span-1 transition-transform duration-300 ease-in-out hover:scale-105 cursor-pointer'>
 						<Image
 							width={90}
@@ -49,7 +60,7 @@ const Trusted = () => {
 					</div>
 				</div>
 			</div>
-		</div>
+		</motion.div>
 	)
 }
 
