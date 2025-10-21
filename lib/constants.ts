fe56f0005ce2490fc3e2d115877ef02510f3c4fa
Jeff Hogg <jeffhogg86@gmail.com>
2025-10-20 19:28:55 -0500

    remove youtube

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,12 +1,4 @@
 export const linkedInUrl = 'https://www.linkedin.com/in/jeffreyehogg/'
 export const githubUrl = 'http://github.com/jeffreyehogg'
 export const twitterUrl = 'https://twitter.com/jeffreyehogg'
-export const youtubeUrl = 'https://www.youtube.com/c/jeffreyehogg'
 
-export const channelId = 'UCSTOp5FPjNXBmop3o47yZaQ'
-export const uploadsId = 'UUSTOp5FPjNXBmop3o47yZaQ'
-export const defaultVid = 'yWuRDTg3VYo'
-export const playlistId = 'PLQnhPm2EJkhbo0nZHOblRzlQIzPhIMXvd'
-export const YOUTUBE_PLAYLISTS_API = `https://www.googleapis.com/youtube/v3/playlists?part=snippet&channelId=${channelId}`
-export const YOUTUBE_PLAYLIST_ITEMS_API = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50`
-export const YOUTUBE_ALL_VIDEOS_API = `https://www.googleapis.com/youtube/v3/playlistItems?playlistId=${uploadsId}&part=snippet&maxResults=50`
