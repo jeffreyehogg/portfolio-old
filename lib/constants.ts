2b40df324ccbb196e81cdbea8589caa0b6579f5d
Jeff Hogg <jeffhogg86@gmail.com>
2025-10-20 19:48:23 -0500

    remove youtube lists

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,3 @@
 export const linkedInUrl = 'https://www.linkedin.com/in/jeffreyehogg/'
 export const githubUrl = 'http://github.com/jeffreyehogg'
 export const twitterUrl = 'https://twitter.com/jeffreyehogg'
-
